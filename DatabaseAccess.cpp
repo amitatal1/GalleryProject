9a4166863623bd79229b4966a526292e42f9fb74
Amit Atal <amitatal1977@gmail.com>
2024-03-16 23:51:24 +0200

    most of part 2 commands

--- DatabaseAccess.cpp
+++ DatabaseAccess.cpp
@@ -16,67 +16,218 @@ bool DatabaseAccess::open()
 	}
 	if (file_exist != 0)
 	{
-		createTables(db);
+		createTables();
 		return true;
 	}
-
-
-
+    
 }
 
 void DatabaseAccess::close()
 {
+    sqlite3_close(db);
 	db = nullptr;
-	sqlite3_close(db);
+}
+
+const std::list<Album> DatabaseAccess::getAlbums()
+{
+    return std::list<Album>();
+}
+
+const std::list<Album> DatabaseAccess::getAlbumsOfUser(const User& user)
+{
+    return std::list<Album>();
+}
+
+void DatabaseAccess::createAlbum(const Album& album)
+{
+    
+}
+
+
+void DatabaseAccess::deleteAlbum(const std::string& albumName, int userId)
+{
+    std::string stringSql =
+    "DELETE FROM Albums WHERE name = "+albumName+" and User_id =" + std::to_string(userId)+";";
+
+    const char* sqlStatement = stringSql.c_str();
+    int res = sqlite3_exec(db, sqlStatement, nullptr, nullptr, nullptr);
+    if (res != SQLITE_OK) {
+        std::cerr << "Error executing statement: " << sqlite3_errmsg(db) << std::endl;
+    }
+}
+
+bool DatabaseAccess::doesAlbumExists(const std::string& albumName, int userId)
+{
+    return false;
+}
+
+Album DatabaseAccess::openAlbum(const std::string& albumName)
+{
+    return Album();
+}
+
+void DatabaseAccess::closeAlbum(Album& pAlbum)
+{
+}
+
+void DatabaseAccess::printAlbums()
+{
+}
+
+void DatabaseAccess::addPictureToAlbumByName(const std::string& albumName, const Picture& picture)
+{
+}
+
+void DatabaseAccess::removePictureFromAlbumByName(const std::string& albumName, const std::string& pictureName)
+{
+    
+}
+
+void DatabaseAccess::tagUserInPicture(const std::string& albumName, const std::string& pictureName, int userId)
+{
+    std::string stringSql =
+        "INSERT INTO Tags (USER_ID, PICTURE_ID) "
+        "SELECT "+std::to_string(userId)+ ", Pictures.ID FROM Pictures "
+        "INNER JOIN Albums ON Pictures.ALBUM_ID = Albums.ID "
+        "WHERE Albums.NAME = "+albumName+"  AND Pictures.NAME = "+ pictureName + "; ";
+
+    const char* sqlStatement = stringSql.c_str();
+    int res = sqlite3_exec(db, sqlStatement, nullptr, nullptr, nullptr);
+    if (res != SQLITE_OK) {
+        std::cerr << "Error executing statement: " << sqlite3_errmsg(db) << std::endl;
+    }
+
+}
+
+void DatabaseAccess::untagUserInPicture(const std::string& albumName, const std::string& pictureName, int userId)
+{
+    std::string stringSql =
+        "DELETE FROM Tags WHERE USER_ID = " + std::to_string(userId) + " AND PICTURE_ID IN "
+        "(SELECT Pictures.ID FROM Pictures "
+        "INNER JOIN Albums ON Pictures.ALBUM_ID = Albums.ID "
+        "WHERE Albums.NAME = '" + albumName + "' AND Pictures.NAME = '" + pictureName + "');";
+
+
+    const char* sqlStatement = stringSql.c_str();
+    int res = sqlite3_exec(db, sqlStatement, nullptr, nullptr, nullptr);
+    if (res != SQLITE_OK) {
+        std::cerr << "Error executing statement: " << sqlite3_errmsg(db) << std::endl;
+    }
+}
+
+void DatabaseAccess::printUsers()
+{
+}
+
+User DatabaseAccess::getUser(int userId)
+{
+}
+
+void DatabaseAccess::createUser(User& user)
+{
+    std::string stringSql =
+        "INSERT INTO USERS (id,name) values("+std::to_string(user.getId())+","+user.getName() + ")"; // ,aybe you shoukd enter the id to the user and not the oppostie, bo awarew to that (past Amit)
+
+
+    const char* sqlStatement = stringSql.c_str();
+    int res = sqlite3_exec(db, sqlStatement, nullptr, nullptr, nullptr);
+    if (res != SQLITE_OK) {
+        std::cerr << "Error executing statement: " << sqlite3_errmsg(db) << std::endl;
+    }
+}
+
+void DatabaseAccess::deleteUser(const User& user)
+{
+
+    std::string stringSql =
+        "DELETE FROM USERS where id=" + std::to_string(user.getId()) + ";";
+
+
+    const char* sqlStatement = stringSql.c_str();
+    int res = sqlite3_exec(db, sqlStatement, nullptr, nullptr, nullptr);
+    if (res != SQLITE_OK) {
+        std::cerr << "Error executing statement: " << sqlite3_errmsg(db) << std::endl;
+    }
+}
+
+bool DatabaseAccess::doesUserExists(int userId)
+{
+    return false;
+}
+
+int DatabaseAccess::countAlbumsOwnedOfUser(const User& user)
+{
+    return 0;
+}
+
+int DatabaseAccess::countAlbumsTaggedOfUser(const User& user)
+{
+    return 0;
+}
+
+int DatabaseAccess::countTagsOfUser(const User& user)
+{
+    return 0;
+}
+
+float DatabaseAccess::averageTagsPerAlbumOfUser(const User& user)
+{
+    return 0.0f;
+}
+
+User DatabaseAccess::getTopTaggedUser()
+{
+}
+
+Picture DatabaseAccess::getTopTaggedPicture()
+{
+}
+
+std::list<Picture> DatabaseAccess::getTaggedPicturesOfUser(const User& user)
+{
+    return std::list<Picture>();
 }
 
 void DatabaseAccess::createTables()
 {
-	const char* sqlStatement = // creating users table
-		"CREATE TABLE if not exists Users \
-		(ID INTEGER PRIMARY\
-		KEY AUTOINCREMENT NOT NULL,\
-		NAME TEXT NOT NULl); ";
+    const char* sqlStatement = // creating users table
+        "CREATE TABLE if not exists Users \
+        (ID INTEGER PRIMARY\
+        KEY AUTOINCREMENT NOT NULL,\
+        NAME TEXT NOT NULL); \
+        \
+        CREATE TABLE if not exists Albums (\
+        ID INTEGER PRIMARY\
+        KEY AUTOINCREMENT NOT NULL,\
+        NAME TEXT NOT NULL,\
+        CREATION_DATE DATETIME NOT NULL,\
+        USER_ID INTEGER NOT NULL,\
+        FOREIGN KEY (USER_ID)\
+        REFERENCES Users(ID)); \
+        \
+        CREATE TABLE if not exists Pictures (\
+        ID INTEGER PRIMARY\
+        KEY AUTOINCREMENT NOT NULL,\
+        NAME TEXT NOT NULL,\
+        LOCATION TEXT NOT NULL,\
+        CREATION_DATE DATETIME NOT NULL,\
+        ALBUM_ID INTEGER NOT NULL,\
+        FOREIGN KEY (ALBUM_ID)\
+        REFERENCES Albums(ID)); \
+        \
+        CREATE TABLE if not exists Tags (\
+        ID INTEGER PRIMARY\
+        KEY AUTOINCREMENT NOT NULL,\
+        USER_ID INTEGER NOT NULL,\
+        PICTURE_ID INTEGER NOT NULL,\
+        FOREIGN KEY (PICTURE_ID)\
+        REFERENCES Pictures(ID)); ";
 
-	char* errMessage = nullptr;
-	int res = sqlite3_exec(db, sqlStatement, nullptr, nullptr, &errMessage);
-	std::cout << res;
-	sqlStatement =
-		"CREATE TABLE if not exists Albums (\
-		(ID INTEGER PRIMARY\
-		KEY AUTOINCREMENT NOT NULL,\
-		NAME TEXT NOT NULl\
-		CREATION_DATE DATETIME  NOT NULL\
-		USER_ID integer not null\
-		FOREIGN KEY (USER_ID)\
-		REFERENCES Users(ID)); ";
-
-	res = sqlite3_exec(db, sqlStatement, nullptr, nullptr, &errMessage);
-	std::cout << res;
-
-	sqlStatement =
-		"CREATE TABLE if not exists Pictures (\
-		(ID INTEGER PRIMARY\
-		KEY AUTOINCREMENT NOT NULL,\
-		NAME TEXT NOT NULl\
-		LOCATION TEXT NOT NULL\
-		CREATION_DATE DATETIME  NOT NULL\
-		ALBUM_ID integer not null\
-		FOREIGN KEY (ALBUM_ID)\
-		REFERENCES Albums(ID)); ";
-
-	res = sqlite3_exec(db, sqlStatement, nullptr, nullptr, &errMessage);
-	std::cout << res;
-
-	sqlStatement =
-		"CREATE TABLE if not exists Pictures (\
-		(ID INTEGER PRIMARY\
-		KEY AUTOINCREMENT NOT NULL,\
-		USER_ID integer NOT NULl\
-		PICTURE_ID integer not null\
-		FOREIGN KEY (ALBUM_ID)\
-		REFERENCES Albums(ID)); ";
-
-	res = sqlite3_exec(db, sqlStatement, nullptr, nullptr, &errMessage);
-	std::cout << res;
+    int res = sqlite3_exec(db, sqlStatement, nullptr, nullptr,nullptr);
+    
+    if (res != SQLITE_OK) 
+    {
+        std::cerr << "Error executing statement: " << sqlite3_errmsg(db) << std::endl;
+    }
 }
+
--- DatabaseAccess.h
+++ DatabaseAccess.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "IDataAccess.h"
 #include "sqlite3.h"
+#include "Album.h"
+
 class DatabaseAccess
 	:public IDataAccess
 {
@@ -8,7 +10,45 @@ public:
 	virtual bool open() override;
 	virtual void close() override ;
 	virtual void clear() override {};
+
+	virtual const std::list<Album> getAlbums() override;
+	virtual const std::list<Album> getAlbumsOfUser(const User& user) override;
+	virtual void createAlbum(const Album& album) override;
+	virtual void deleteAlbum(const std::string& albumName, int userId) override;
+	virtual bool doesAlbumExists(const std::string& albumName, int userId) override;
+	virtual Album openAlbum(const std::string& albumName) override;
+	virtual void closeAlbum(Album& pAlbum) override;
+	virtual void printAlbums() override;
+
+    // picture related
+	virtual void addPictureToAlbumByName(const std::string& albumName, const Picture& picture) override;
+	virtual void removePictureFromAlbumByName(const std::string& albumName, const std::string& pictureName) override;
+	virtual void tagUserInPicture(const std::string& albumName, const std::string& pictureName, int userId) override;
+	virtual void untagUserInPicture(const std::string& albumName, const std::string& pictureName, int userId) override;
+
+	// user related
+	virtual void printUsers() =0;
+	virtual User getUser(int userId) override;
+	virtual void createUser(User& user ) override;
+	virtual void deleteUser(const User& user) override;
+	virtual bool doesUserExists(int userId) override ;
+	
+
+	// user statistics
+	virtual int countAlbumsOwnedOfUser(const User& user) override;
+	virtual int countAlbumsTaggedOfUser(const User& user) override;
+	virtual int countTagsOfUser(const User& user) override;
+	virtual float averageTagsPerAlbumOfUser(const User& user) override;
+
+	// queries
+	virtual User getTopTaggedUser() override;
+	virtual Picture getTopTaggedPicture() override;
+	virtual std::list<Picture> getTaggedPicturesOfUser(const User& user) override;
+	
+
 private:
+
+
 	void createTables();
 	sqlite3* db;
 };
